619a95bc3703a0aabba295a20da6514141eaf627
Jakub Czerniak <jakubczerniak85@gmail.com>
2026-06-17 18:40:50 +0200

    init - projekt Flutter Wizarding World

---
+++ android/app/src/main/kotlin/com/example/projekt_zaliczeniowy_jc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projekt_zaliczeniowy_jc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wizarding World',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wizarding World'),
+      ),
+      body: const Center(
+        child: Text('Lista postaci'),
+      ),
+    );
+  }
+}
